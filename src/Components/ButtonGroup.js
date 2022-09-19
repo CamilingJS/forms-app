01dc0d557519f25c2642433ae295faabbb6578cd
CamilingJS <jonathancamiling@gmail.com>
2022-09-18 19:24:20 -0700

    in ButtonGroup component we created state for selectedButton we created an array of ALT_STYLES and BUTTON_TEXT. We map through BUTTON_TEXT add as an indicator and setSelectedButton(i)

--- src/Components/ButtonGroup.js
+++ src/Components/ButtonGroup.js
@@ -1,8 +1,40 @@
+import { useState } from "react"
+
+// .ButtonGroup > .selected-button {
+//     color: teal; 
+//     background-color: white;
+//     transition: .5s ease-in;
+//   }
+
+const ALT_STYLES = {
+    color: 'teal',
+    background: 'white'
+}
+
+const BUTTON_TEXT = [
+    '$100-$1K', '$1K-$5K', 'No limit'
+]
+
 export default function ButtonGroup(){
+
+    const [selectedButton, setSelectedButton] = useState(0)
+
 
     return(
         <div className="ButtonGroup">
-            <button>Next</button>
+            
+            {/* <button onClick={()=>setSelectedButton(0)} style={ selectedButton === 0 ? ALT_STYLES : {} } >$100-$1K</button>
+            <button onClick={()=>setSelectedButton(1)} style={ selectedButton === 1 ? ALT_STYLES : {} } >$1K-$5K</button>
+            <button onClick={()=>setSelectedButton(2)} style={ selectedButton === 2 ? ALT_STYLES : {} } >No limit</button> */}
+
+            {
+                BUTTON_TEXT.map((btnText, i) => (
+                    <button onClick={()=>setSelectedButton(i)} style={ selectedButton === i ? ALT_STYLES : {} } >{btnText}</button>
+                )
+                    
+                )
+            }
+            
         </div>
     )
 }
