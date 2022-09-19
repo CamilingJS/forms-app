62f7c06905f781d73bcb812ba24ccaed86f150fc
CamilingJS <jonathancamiling@gmail.com>
2022-09-18 21:59:39 -0700

    in TextInput component I am using onChange to use the seters to update the e.target.value of the inputs.

--- src/Components/TextInput.js
+++ src/Components/TextInput.js
@@ -1,22 +1,28 @@
 import ButtonGroup from "./ButtonGroup"
 
-export default function TextInput({stage, setStage}){
+export default function TextInput({stage, updateEmail, email, buttonData, setButtonData, updateDetails, details}){
 
     let content; 
     if (stage === 0){
         content = (
-            <input type="text" placeholder="What's your email?" /> 
+            <input 
+            onChange={e => updateEmail(e.target.value)}
+            type="text" 
+            placeholder="What's your email?" /> 
         )
     } else if (stage === 1) {
         content = (
-            <ButtonGroup />
+            <ButtonGroup buttonData={buttonData} setButtonData={setButtonData} />
+          
         )
         
     } else if (stage === 2){
         content = (
-            <input type="text" /> 
+            <input onChange={e => updateDetails(e.target.value)} type="text" /> 
         )
         
+    } else if (stage === 3){
+        alert(JSON.stringify({email,details,buttonData}))
     }
 
     return(
